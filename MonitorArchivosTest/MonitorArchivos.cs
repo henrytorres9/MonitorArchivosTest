faa2d9ba951640ecd907a42145ff4549d518439d
henrytorres9 <hentores9@gmail.com>
2023-04-07 17:12:05 -0600

    Agregar archivos de proyecto.

---
+++ MonitorArchivosTest/MonitorArchivos.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace MonitorArchivosTest
+{
+    [RunInstaller(true)]
+
+    public partial class MonitorArchivos : ServiceBase
+    {
+        private FileSystemWatcher fileSystemWatcher;
+
+        public MonitorArchivos()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            fileSystemWatcher = new FileSystemWatcher();
+            fileSystemWatcher.Path = "C:\\Test";
+            fileSystemWatcher.NotifyFilter = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            fileSystemWatcher.Filter = "*.*";
+            fileSystemWatcher.IncludeSubdirectories = true;
+            fileSystemWatcher.Changed += new FileSystemEventHandler(OnChanged);
+            fileSystemWatcher.Created += new FileSystemEventHandler(OnChanged);
+            fileSystemWatcher.Deleted += new FileSystemEventHandler(OnChanged);
+            fileSystemWatcher.Renamed += new RenamedEventHandler(OnRenamed);
+            fileSystemWatcher.EnableRaisingEvents = true;
+        }
+
+        private void OnChanged(object source, FileSystemEventArgs e)
+        {
+            // Registra los cambios en el archivo de log
+            LogToFile($"{e.ChangeType} {e.FullPath}");
+        }
+
+        private void OnRenamed(object source, RenamedEventArgs e)
+        {
+            // Registra los cambios en el archivo de log
+            LogToFile($"Archivo {e.OldFullPath} renombrado a: {e.FullPath}");
+        }
+
+        private void LogToFile(string message)
+        {
+            string logFilePath = "D:\\Documentos\\Videos\\Captures\\log.txt";
+            using (StreamWriter streamWriter = new StreamWriter(logFilePath, true))
+            {
+                streamWriter.WriteLine($"{DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")} - {message}");
+            }
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
---
+++ MonitorArchivosTest/ProjectInstaller.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace MonitorArchivosTest
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller1 = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller1
+            // 
+            this.serviceProcessInstaller1.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.serviceProcessInstaller1.Password = null;
+            this.serviceProcessInstaller1.Username = null;
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.Description = "Monitorea todos los eventos de cambios ocurridos en el directorio: 192.168.200.8\\" +
+    "Facturacion, y crea un archivo log de los cambios en tiempo real en el directori" +
+    "o: 192.168.200.8\\ctrldoc.";
+            this.serviceInstaller1.DisplayName = "Monitor de archivos";
+            this.serviceInstaller1.ServiceName = "MonitorArchivos";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller1,
+            this.serviceInstaller1});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller1;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
+    }
+}
